bd4760c301143253228f250aa2c218a96d8be6fd
kd41 <aleksei.mahhov@webmedia.ee>
2012-06-26 20:02:15 +0300

    added new firat files

---
+++ src/suncertify/db/DuplicateKeyException.java
@@ -0,0 +1,12 @@
+package suncertify.db;
+
+public class DuplicateKeyException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public DuplicateKeyException() {
+  }
+
+  public DuplicateKeyException(String message) {
+  }
+
+}
---
+++ src/suncertify/db/RecordNotFoundException.java
@@ -0,0 +1,11 @@
+package suncertify.db;
+
+public class RecordNotFoundException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public RecordNotFoundException() {
+  }
+
+  public RecordNotFoundException(String message) {
+  }
+}
