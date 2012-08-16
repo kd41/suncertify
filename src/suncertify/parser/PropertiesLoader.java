c9cbcc2ef129da3e856b1e21734d9981b3f235ff
kd41 <aleksei.mahhov@webmedia.ee>
2012-08-16 15:10:48 +0300

    sencertify.properties search from root folder; RMI simple example

--- src/suncertify/parser/PropertiesLoader.java
+++ src/suncertify/parser/PropertiesLoader.java
@@ -1,14 +1,21 @@
 package suncertify.parser;
 
-import java.util.ResourceBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PropertiesLoader {
+  private static final Logger log = LoggerFactory.getLogger(PropertiesLoader.class);
+
   private boolean isAloneMode = true;
   private static PropertiesLoader instance;
-  private static final String RESOURCE_FILE_NAME = "suncertify";
+  private static final String RESOURCE_FILE_NAME = "suncertify.properties";
   private static final String DB_ALONE_LOCATION_KEY = "db.alone.location";
   private static final String DB_SERVER_LOCATION_KEY = "db.server.location";
-  private static ResourceBundle resourceBundle;
+  private static Properties resourceBundle;
   private String dbLocation;
 
   private PropertiesLoader() {
@@ -17,7 +24,21 @@ public class PropertiesLoader {
   public static synchronized PropertiesLoader getInstance() {
     if (instance == null) {
       instance = new PropertiesLoader();
-      resourceBundle = ResourceBundle.getBundle(RESOURCE_FILE_NAME);
+      resourceBundle = new Properties();
+      FileInputStream is = null;
+      try {
+        is = new FileInputStream(RESOURCE_FILE_NAME);
+        resourceBundle.load(is);
+      } catch (IOException e) {
+        log.error(e.getMessage(), e);
+      } finally {
+        if (null != is) {
+          try {
+            is.close();
+          } catch (IOException e) {
+          }
+        }
+      }
     }
     return instance;
   }
@@ -25,9 +46,9 @@ public class PropertiesLoader {
   public String getDbLocation() {
     if (this.dbLocation == null) {
       if (isAloneMode) {
-        dbLocation = resourceBundle.getString(DB_ALONE_LOCATION_KEY);
+        dbLocation = resourceBundle.getProperty(DB_ALONE_LOCATION_KEY);
       } else {
-        dbLocation = resourceBundle.getString(DB_SERVER_LOCATION_KEY);
+        dbLocation = resourceBundle.getProperty(DB_SERVER_LOCATION_KEY);
       }
     }
     return dbLocation;
---
+++ src/suncertify/rmi/client/Client.java
@@ -0,0 +1,26 @@
+package suncertify.rmi.client;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import suncertify.rmi.server.Hello;
+
+public class Client {
+
+  private Client() {
+  }
+
+  public static void main(String[] args) {
+
+    String host = (args.length < 1) ? null : args[0];
+    try {
+      Registry registry = LocateRegistry.getRegistry(1024);
+      Hello stub = (Hello) registry.lookup("Hello");
+      String response = stub.sayHello();
+      System.out.println("response: " + response);
+    } catch (Exception e) {
+      System.err.println("Client exception: " + e.toString());
+      e.printStackTrace();
+    }
+  }
+}
